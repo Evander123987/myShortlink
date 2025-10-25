09f9c081b1657c63b82337aa82943519c6d7d156
evander <tangxu123987@gmail.com>
2025-10-25 09:59:11 +0800

    feature:开发短链接后管分组新增功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/GroupDO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/GroupDO.java
@@ -1,9 +1,11 @@
 package com.nageoffer.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+import com.nageoffer.shortlink.admin.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -12,7 +14,10 @@ import java.util.Date;
  */
 @Data
 @TableName("t_group")
-public class GroupDO {
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupDO extends BaseDO {
     private static final long serialVersionUID = 1L;
 
     @TableId(type = IdType.AUTO)
@@ -40,19 +45,4 @@ public class GroupDO {
      * 分组排序
      */
     private Integer sortOrder;
-
-    /**
-     * 创建时间
-     */
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    private Integer delFlag;
 }
---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/req/ShortLinkGroupSaveReqDTO.java
@@ -0,0 +1,15 @@
+package com.nageoffer.shortlink.admin.dto.req;
+
+import lombok.Data;
+
+/**
+ * 短链接分组
+ */
+@Data
+public class ShortLinkGroupSaveReqDTO {
+    /**
+     * 分组名
+     */
+    private String name;
+
+}
